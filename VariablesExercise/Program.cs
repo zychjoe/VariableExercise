1ad897f1988a53259e69ceab6a1298b88ed2752a
Joe Zedd <zychjoe@gmail.com>
2022-02-03 11:25:00 -0500

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string partner = "Christina";
+            int age = 28;
+            char icon = '*';
+            bool inLove = true;
+            double relationshipLenYear = .9;
+            decimal relationshipLenMo = 11.1m;
+
+
+            Console.WriteLine($"Hi! my partner's name is {partner}. " +
+                $"She is {age} years old. " +
+                $"She is a {icon}! " +
+                $"If people ask if we're in love, I say {inLove}! " +
+                $"We have been together for {relationshipLenYear} years. " +
+                $"Which is actually {relationshipLenMo} months. ");
+        }
+    }
+}
